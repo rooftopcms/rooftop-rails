05ca63adb7a128ee3741f101bfb61e572a3b4c03
Ed Jones <ed@errorstudio.co.uk>
2015-12-11 16:37:30 +0000

    Add collection caching and clearing - woo.

--- app/models/concerns/rooftop/rails/object_cache.rb
+++ app/models/concerns/rooftop/rails/object_cache.rb
@@ -25,7 +25,7 @@ module Rooftop
             uncached_ids = ids.reject {|i| ::Rails.cache.read("#{cache_key_base}/#{i}").present?}
             [super(*uncached_ids)].flatten.reject(&:blank?).collect do |object|
               ::Rails.logger.debug("Caching #{cache_key_base}/#{object.id}")
-              ::Rails.cache.write("#{cache_key_base}/#{object.id}",object)
+              ::Rails.cache.write(object.cache_key,object)
             end
             all_objects = ids.collect do |id|
               ::Rails.cache.read("#{cache_key_base}/#{id}")
@@ -35,6 +35,59 @@ module Rooftop
           else
             super
           end
+        end
+
+        def where(args)
+          #Sort the arguments, and any keys which are arrays
+          args = Hash[args.sort.each {|k,v| v.sort! if v.is_a?(Array)}]
+          # Generate a hash for keying the cache of the results
+          args_hash = Digest::MD5.hexdigest(args.to_s)
+          cache_key = "#{cache_key_base}/collection_query/#{args_hash}"
+          # first see if we have a collection which matches the args
+          cached_collection = ::Rails.cache.read(cache_key)
+          # if it's present, then we can return it directly.
+          if cached_collection.present?
+            return cached_collection
+          else
+            # If not, then we need to call super() to get it from the API
+            collection = super(args)
+            # and write it into the cache
+            ::Rails.cache.write(cache_key,collection)
+            # We also iterate over the collection and cache each object, and cache the argument hash against each object
+            collection.each do |object|
+              # This is a bit funky and circular. We store an array of hashes for queries to which this object belongs.
+              # If the object cache needs to be removed, we can iterate through those hashes and clear the collection caches too.
+              collection_query_hash_key = "#{object.cache_key}/collection_hashes"
+              collection_hashes = ::Rails.cache.read(collection_query_hash_key) || []
+              collection_hashes << args_hash
+              ::Rails.cache.write(collection_query_hash_key, collection_hashes)
+              # this is the object cache - i.e. it'll respond with a cache lookup for Page.find(14) or whatever
+              ::Rails.cache.write(object.cache_key,object)
+            end
+            collection
+          end
+        end
+
+        # A method to expire the relevant caches for a collection of objects or ids
+        # @param args [Array] of either objects which respond to `.id`, or ids themselves
+        def expire_cache_for(*args)
+          args = args.collect {|a| a.respond_to?(:id) ? a.id : a}
+          # the caches we need to clear are:
+          # - the object cache
+          # - any collection caches which included this object
+          args.each do |id|
+            object_cache_key = "#{cache_key_base}/#{id}"
+            collection_keys = (::Rails.cache.read("#{object_cache_key}/collection_hashes") || []).collect do |hash|
+              "#{cache_key_base}/collection_query/#{hash}"
+            end
+            collection_keys << "#{object_cache_key}/collection_hashes"
+            [collection_keys + [object_cache_key]].flatten.each do |key|
+              ::Rails.cache.delete(key)
+            end
+
+          end
+
+
         end
 
       end
