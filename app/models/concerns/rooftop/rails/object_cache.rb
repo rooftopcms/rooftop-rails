f0ff080c09cf0cfee924caeed51c941db96a9939
Ed Jones <ed@errorstudio.co.uk>
2016-03-10 15:09:56 +0000

    Clear the cache harder when a new object is created - in other words we don't have it in any collections

--- app/models/concerns/rooftop/rails/object_cache.rb
+++ app/models/concerns/rooftop/rails/object_cache.rb
@@ -129,13 +129,28 @@ module Rooftop
           # - any collection caches which included this object
           args.each do |id|
             object_cache_key = "#{cache_key_base}/#{id}"
-            collection_keys = (::Rails.cache.read("#{object_cache_key}/collection_hashes") || []).collect do |hash|
-              "#{cache_key_base}/collection_query/#{hash}"
+            object_collections = ::Rails.cache.read("#{object_cache_key}/collection_hashes")
+            if object_collections.present?
+              # this object has a list of collection hashes, each of which we need to remove
+              object_collections.each do |hash|
+                ::Rails.cache.delete("#{cache_key_base}/collection_query/#{hash}")
+              end
+              # by implication, the cached object should also be present; clear that too
+              ::Rails.cache.delete(object_cache_key)
+            else
+              # the object isn't in any collections; unfortunately we can't be sure whether this is because the object is old
+              # and uncached, or new and therefore needs to be in collections which currently exist for this class.
+              # Because of that, we'll aggressively remove caches for collections
+              begin
+                ::Rails.cache.delete_matched("#{cache_key_base}/collection_query*")
+              rescue NotImplementedError
+                ::Rails.cache.clear # rescue to Rails.cache.clear for caching methods where delete_matched isn't supported
+              end
             end
-            collection_keys << "#{object_cache_key}/collection_hashes"
-            [collection_keys + [object_cache_key]].flatten.each do |key|
-              ::Rails.cache.delete(key)
-            end
+            # Always remove the object's cache. There's no risk of doing this for nonexistent things.
+            ::Rails.cache.delete(object_cache_key)
+
+
           end
         end
       end
