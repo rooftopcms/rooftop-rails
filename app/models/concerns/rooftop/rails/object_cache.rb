f1d28eb9e60dbb3d45c5a889486ea762dcec1bce
Ed Jones <ed@errorstudio.co.uk>
2018-05-18 14:31:59 +0100

    Ensure object cache now uses an array rather than a Her::Model::Relation when trying to cache (the latter is not dumpable)

--- app/models/concerns/rooftop/rails/object_cache.rb
+++ app/models/concerns/rooftop/rails/object_cache.rb
@@ -63,7 +63,7 @@ module Rooftop
               return cached_collection
             else
               # If not, then we need to call super() to get it from the API
-              collection = super(args)
+              collection = super(args).to_a
               # and write it into the cache
               ::Rails.cache.write(cache_key,collection)
               # We also iterate over the collection and cache each object, and cache the argument hash against each object
@@ -104,7 +104,7 @@ module Rooftop
               return cached_collection
             else
               # If not, then we need to call super() to get it from the API
-              collection = super
+              collection = super.to_a
               # and write it into the cache
               ::Rails.cache.write(cache_key,collection)
               # We also iterate over the collection and cache each object, and cache the argument hash against each object
